a35a4bf19e13ee975d34fad1f971b450108d5ef5
Martin Nijboer <m.r.nijboer@thables.com>
2022-08-18 21:06:03 +0200

    Add top-level field type validation

--- lib/goal.ex
+++ lib/goal.ex
@@ -12,7 +12,37 @@ defmodule Goal do
       :world
 
   """
-  def hello do
-    :world
+  def validate(params, schema) do
+    data = %{}
+    types = get_types(schema)
+
+    changeset =
+      {data, types}
+      |> Ecto.Changeset.cast(params, Map.keys(types))
+
+    case changeset do
+      %Ecto.Changeset{valid?: false} = changeset -> {:error, changeset}
+      %Ecto.Changeset{valid?: true, changes: changes} -> {:ok, changes}
+    end
+  end
+
+  defp get_types(schema) do
+    Enum.reduce(schema, %{}, fn {field, rules}, acc ->
+      case Keyword.get(rules, :type, :string) do
+        :enum ->
+          values =
+            rules
+            |> Keyword.get(:values, [])
+            |> Enum.map(&String.to_atom/1)
+
+          Map.put(acc, field, {:parameterized, Ecto.Enum, Ecto.Enum.init(values: values)})
+
+        :list ->
+          Map.put(acc, field, {:array, Keyword.get(rules, :inner_type, :string)})
+
+        type ->
+          Map.put(acc, field, type)
+      end
+    end)
   end
 end
--- test/goal_test.exs
+++ test/goal_test.exs
@@ -1,8 +1,90 @@
 defmodule GoalTest do
   use ExUnit.Case
-  doctest Goal
 
-  test "greets the world" do
-    assert Goal.hello() == :world
+  describe "validate/2" do
+    test "example" do
+      data = %{
+        "id" => "1",
+        "uuid" => "f45fb959-b0f9-4a32-b6ca-d32bdb53ee8e",
+        "string" => "Daisy",
+        "trimmed_string" => " human ",
+        "squished_string" => " homo  sapien ",
+        "integer" => 29,
+        "list" => ["1", "2"],
+        "integer_list" => [1, 2],
+        "boolean_list" => [true, false],
+        "boolean" => true,
+        "map" => %{
+          "key" => "value"
+        },
+        "nested_map" => %{
+          "key" => "value",
+          "map" => %{
+            "key" => "value"
+          }
+        },
+        "float" => 60.5,
+        "decimal" => 100.04,
+        "email" => "daisy.doe@example.com",
+        "password" => "password123",
+        "enum" => "female",
+        "other" => "not in schema"
+      }
+
+      schema = %{
+        id: [type: :integer],
+        uuid: [type: :string, format: :uuid],
+        string: [type: :string],
+        trimmed_string: [type: :string, trim: true],
+        squished_string: [type: :string, squish: true],
+        integer: [type: :integer],
+        list: [type: :list],
+        integer_list: [type: :list, inner_type: :integer],
+        boolean_list: [type: :list, inner_type: :boolean],
+        boolean: [type: :boolean],
+        map: [type: :map],
+        nested_map: [
+          type: :map,
+          properties: %{
+            key: [type: :string],
+            map: [type: :map]
+          }
+        ],
+        float: [type: :float],
+        decimal: [type: :decimal],
+        email: [type: :string, format: :email],
+        password: [type: :string, format: :password],
+        enum: [type: :enum, values: ["male", "female", "non-binary"]]
+      }
+
+      assert Goal.validate(data, schema) ==
+               {:ok,
+                %{
+                  id: 1,
+                  uuid: "f45fb959-b0f9-4a32-b6ca-d32bdb53ee8e",
+                  string: "Daisy",
+                  trimmed_string: "human",
+                  squished_string: "homo sapien",
+                  integer: 29,
+                  list: ["1", "2"],
+                  integer_list: [1, 2],
+                  boolean_list: [true, false],
+                  boolean: true,
+                  map: %{
+                    "key" => "value"
+                  },
+                  nested_map: %{
+                    key: "value",
+                    map: %{
+                      key: "value"
+                    }
+                  },
+                  float: 60.5,
+                  decimal: Decimal.from_float(100.04),
+                  email: "daisy.doe@example.com",
+                  password: "password123",
+                  enum: :female
+                }}
+    end
   end
 end
