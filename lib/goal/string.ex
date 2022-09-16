6e583b9b5805137ad6fb790e6680b0ae31d9edf8
Martin Nijboer <m.r.nijboer@thables.com>
2022-09-17 01:31:18 +0200

    Add Goal.String

---
+++ lib/goal/string.ex
@@ -0,0 +1,21 @@
+defmodule Goal.String do
+  @moduledoc """
+  Defines string operations.
+  """
+
+  @doc """
+  Returns a string where all leading, trailing, and excessive inner Unicode whitespaces have been removed.
+
+  ## Examples
+
+      iex> squish(" banana  man ")
+      "banana man"
+
+  """
+  @spec squish(binary) :: binary
+  def squish(string) do
+    string
+    |> String.split()
+    |> Enum.join(" ")
+  end
+end
