341a9178d575195dad815aceed821ac53f179eed
Martin Nijboer <m.r.nijboer@thables.com>
2022-09-17 01:31:41 +0200

    Add string and integer validations

--- lib/goal.ex
+++ lib/goal.ex
@@ -3,13 +3,24 @@ defmodule Goal do
   Documentation for `Goal`.
   """
 
+  import Ecto.Changeset
+
   alias Ecto.Changeset
 
   @doc """
-  TODO: Add docs
+  Validates the given parameters against the given schema.
+
+  ## Examples
+
+      iex> validate_params(%{"email" => "jane@example.com"}, %{email: [type: :string, format: :email]})
+      {:ok, %{email: "jane@example.com"}}
+
+      iex> validate_params(%{"email" => "invalid"}, %{email: [type: :string, format: :email]})
+      {:error, %Ecto.Changeset{}}
+
   """
-  @spec validate(map, map) :: {:ok, map} | {:error, Changeset.t()}
-  def validate(params, schema) do
+  @spec validate_params(map, map) :: {:ok, map} | {:error, Changeset.t()}
+  def validate_params(params, schema) do
     schema
     |> get_types()
     |> build_changeset(params, schema)
@@ -22,30 +33,58 @@ defmodule Goal do
   defp build_changeset(types, params, schema) do
     {%{}, types}
     |> Ecto.Changeset.cast(params, Map.keys(types))
-    |> validate_complex_fields(types, schema)
+    |> validate_simple_fields(schema)
+    |> validate_nested_fields(types, schema)
   end
 
-  defp get_types(schema) do
-    Enum.reduce(schema, %{}, fn {field, rules}, acc ->
-      case Keyword.get(rules, :type, :string) do
-        :enum ->
-          values =
-            rules
-            |> Keyword.get(:values, [])
-            |> Enum.map(&String.to_atom/1)
+  defp validate_simple_fields(%Ecto.Changeset{changes: changes} = changeset, schema) do
+    Enum.reduce(changes, changeset, fn {field, _value}, changeset_acc ->
+      schema
+      |> Map.get(field, [])
+      |> Enum.reduce(changeset_acc, fn
+        {:trim, true}, inner_acc ->
+          update_change(inner_acc, field, &String.trim/1)
 
-          Map.put(acc, field, {:parameterized, Ecto.Enum, Ecto.Enum.init(values: values)})
+        {:squish, true}, inner_acc ->
+          update_change(inner_acc, field, &Goal.String.squish/1)
 
-        :list ->
-          Map.put(acc, field, {:array, Keyword.get(rules, :inner_type, :string)})
+        {:format, :uuid}, inner_acc ->
+          validate_format(inner_acc, field, Goal.Regex.uuid())
 
-        type ->
-          Map.put(acc, field, type)
-      end
+        {:format, :email}, inner_acc ->
+          validate_format(inner_acc, field, Goal.Regex.email())
+
+        {:format, :password}, inner_acc ->
+          validate_format(inner_acc, field, Goal.Regex.password())
+
+        {:format, :url}, inner_acc ->
+          validate_format(inner_acc, field, Goal.Regex.url())
+
+        {:less_than, integer}, inner_acc ->
+          validate_number(inner_acc, field, less_than: integer)
+
+        {:greater_than, integer}, inner_acc ->
+          validate_number(inner_acc, field, greater_than: integer)
+
+        {:less_than_or_equal_to, integer}, inner_acc ->
+          validate_number(inner_acc, field, less_than_or_equal_to: integer)
+
+        {:greater_than_or_equal_to, integer}, inner_acc ->
+          validate_number(inner_acc, field, greater_than_or_equal_to: integer)
+
+        {:equal_to, integer}, inner_acc ->
+          validate_number(inner_acc, field, equal_to: integer)
+
+        {:not_equal_to, integer}, inner_acc ->
+          validate_number(inner_acc, field, not_equal_to: integer)
+
+        {_name, _setting}, inner_acc ->
+          inner_acc
+      end)
     end)
   end
 
-  defp validate_complex_fields(%Ecto.Changeset{changes: changes} = changeset, types, schema) do
+  defp validate_nested_fields(%Ecto.Changeset{changes: changes} = changeset, types, schema) do
     Enum.reduce(types, changeset, fn
       {field, :map}, acc ->
         inner_params = Map.get(changes, field)
@@ -71,6 +110,26 @@ defmodule Goal do
 
       {_field, _type}, acc ->
         acc
+    end)
+  end
+
+  defp get_types(schema) do
+    Enum.reduce(schema, %{}, fn {field, rules}, acc ->
+      case Keyword.get(rules, :type, :string) do
+        :enum ->
+          values =
+            rules
+            |> Keyword.get(:values, [])
+            |> Enum.map(&String.to_atom/1)
+
+          Map.put(acc, field, {:parameterized, Ecto.Enum, Ecto.Enum.init(values: values)})
+
+        :list ->
+          Map.put(acc, field, {:array, Keyword.get(rules, :inner_type, :string)})
+
+        type ->
+          Map.put(acc, field, type)
+      end
     end)
   end
 end
--- test/goal_test.exs
+++ test/goal_test.exs
@@ -1,8 +1,8 @@
 defmodule GoalTest do
   use ExUnit.Case
 
-  describe "validate/2" do
-    test "example" do
+  describe "validate_params/2" do
+    test "valid example" do
       data = %{
         "id" => "1",
         "uuid" => "f45fb959-b0f9-4a32-b6ca-d32bdb53ee8e",
@@ -10,6 +10,8 @@ defmodule GoalTest do
         "trimmed_string" => " human ",
         "squished_string" => " homo  sapien ",
         "integer" => 29,
+        "min_integer" => 5,
+        "max_integer" => 9,
         "list" => ["1", "2"],
         "integer_list" => [1, 2],
         "boolean_list" => [true, false],
@@ -27,6 +29,7 @@ defmodule GoalTest do
         "decimal" => 100.04,
         "email" => "jane.doe@example.com",
         "password" => "password123",
+        "url" => "https://www.example.com",
         "enum" => "female",
         "other" => "not in schema"
       }
@@ -38,6 +41,8 @@ defmodule GoalTest do
         trimmed_string: [type: :string, trim: true],
         squished_string: [type: :string, squish: true],
         integer: [type: :integer],
+        min_integer: [type: :integer, min: 3],
+        max_integer: [type: :integer, min: 10],
         list: [type: :list],
         integer_list: [type: :list, inner_type: :integer],
         boolean_list: [type: :list, inner_type: :boolean],
@@ -54,10 +59,11 @@ defmodule GoalTest do
         decimal: [type: :decimal],
         email: [type: :string, format: :email],
         password: [type: :string, format: :password],
+        url: [type: :string, format: :url],
         enum: [type: :enum, values: ["male", "female", "non-binary"]]
       }
 
-      assert Goal.validate(data, schema) ==
+      assert Goal.validate_params(data, schema) ==
                {:ok,
                 %{
                   id: 1,
@@ -66,6 +72,8 @@ defmodule GoalTest do
                   trimmed_string: "human",
                   squished_string: "homo sapien",
                   integer: 29,
+                  max_integer: 9,
+                  min_integer: 5,
                   list: ["1", "2"],
                   integer_list: [1, 2],
                   boolean_list: [true, false],
@@ -83,8 +91,159 @@ defmodule GoalTest do
                   decimal: Decimal.from_float(100.04),
                   email: "jane.doe@example.com",
                   password: "password123",
+                  url: "https://www.example.com",
                   enum: :female
                 }}
+    end
+
+    test "string, trim: true" do
+      schema = %{name: [type: :string, trim: true]}
+
+      data_1 = %{"name" => " quantum physics "}
+      data_2 = %{"name" => " quantum  physics "}
+
+      assert Goal.validate_params(data_1, schema) == {:ok, %{name: "quantum physics"}}
+      assert Goal.validate_params(data_2, schema) == {:ok, %{name: "quantum  physics"}}
+    end
+
+    test "string, trim: false" do
+      schema = %{name: [type: :string, trim: false]}
+
+      data_1 = %{"name" => " quantum physics "}
+      data_2 = %{"name" => " quantum  physics "}
+
+      assert Goal.validate_params(data_1, schema) == {:ok, %{name: " quantum physics "}}
+      assert Goal.validate_params(data_2, schema) == {:ok, %{name: " quantum  physics "}}
+    end
+
+    test "string, squish: true" do
+      schema = %{name: [type: :string, squish: true]}
+
+      data = %{"name" => " banana   man "}
+
+      assert Goal.validate_params(data, schema) == {:ok, %{name: "banana man"}}
+    end
+
+    test "string, squish: false" do
+      schema = %{name: [type: :string, squish: false]}
+
+      data = %{"name" => " banana   man "}
+
+      assert Goal.validate_params(data, schema) == {:ok, %{name: " banana   man "}}
+    end
+
+    test "string, format: :uuid" do
+      schema = %{uuid: [type: :string, format: :uuid]}
+
+      data_1 = %{"uuid" => "f45fb959-b0f9-4a32-b6ca-d32bdb53ee8e"}
+      data_2 = %{"uuid" => "notuuid"}
+
+      assert Goal.validate_params(data_1, schema) ==
+               {:ok, %{uuid: "f45fb959-b0f9-4a32-b6ca-d32bdb53ee8e"}}
+
+      assert {:error, %Ecto.Changeset{errors: [uuid: _]}} = Goal.validate_params(data_2, schema)
+    end
+
+    test "string, format: :email" do
+      schema = %{email: [type: :string, format: :email]}
+
+      data_1 = %{"email" => "jane.doe@example.com"}
+      data_2 = %{"email" => "notemail"}
+
+      assert Goal.validate_params(data_1, schema) == {:ok, %{email: "jane.doe@example.com"}}
+
+      assert {:error, %Ecto.Changeset{errors: [email: _]}} = Goal.validate_params(data_2, schema)
+    end
+
+    test "string, format: :password" do
+      schema = %{password: [type: :string, format: :password]}
+
+      data_1 = %{"password" => "password123"}
+      data_2 = %{"password" => "pass"}
+
+      assert Goal.validate_params(data_1, schema) == {:ok, %{password: "password123"}}
+
+      assert {:error, %Ecto.Changeset{errors: [password: _]}} =
+               Goal.validate_params(data_2, schema)
+    end
+
+    test "string, format: :url" do
+      schema = %{url: [type: :string, format: :url]}
+
+      data_1 = %{"url" => "https://www.example.com"}
+      data_2 = %{"url" => "website"}
+
+      assert Goal.validate_params(data_1, schema) == {:ok, %{url: "https://www.example.com"}}
+
+      assert {:error, %Ecto.Changeset{errors: [url: _]}} = Goal.validate_params(data_2, schema)
+    end
+
+    test "integer, less_than: 10" do
+      schema = %{age: [type: :integer, less_than: 11]}
+
+      data_1 = %{"age" => 9}
+      data_2 = %{"age" => 11}
+
+      assert Goal.validate_params(data_1, schema) == {:ok, %{age: 9}}
+
+      assert {:error, %Ecto.Changeset{errors: [age: _]}} = Goal.validate_params(data_2, schema)
+    end
+
+    test "integer, greater_than: 5" do
+      schema = %{age: [type: :integer, greater_than: 5]}
+
+      data_1 = %{"age" => 6}
+      data_2 = %{"age" => 4}
+
+      assert Goal.validate_params(data_1, schema) == {:ok, %{age: 6}}
+
+      assert {:error, %Ecto.Changeset{errors: [age: _]}} = Goal.validate_params(data_2, schema)
+    end
+
+    test "integer, less_than_or_equal_to: 5" do
+      schema = %{age: [type: :integer, less_than_or_equal_to: 5]}
+
+      data_1 = %{"age" => 5}
+      data_2 = %{"age" => 4}
+      data_3 = %{"age" => 6}
+
+      assert Goal.validate_params(data_1, schema) == {:ok, %{age: 5}}
+      assert Goal.validate_params(data_2, schema) == {:ok, %{age: 4}}
+      assert {:error, %Ecto.Changeset{errors: [age: _]}} = Goal.validate_params(data_3, schema)
+    end
+
+    test "integer, greater_than_or_equal_to: 10" do
+      schema = %{age: [type: :integer, greater_than_or_equal_to: 11]}
+
+      data_1 = %{"age" => 11}
+      data_2 = %{"age" => 12}
+      data_3 = %{"age" => 9}
+
+      assert Goal.validate_params(data_1, schema) == {:ok, %{age: 11}}
+      assert Goal.validate_params(data_2, schema) == {:ok, %{age: 12}}
+      assert {:error, %Ecto.Changeset{errors: [age: _]}} = Goal.validate_params(data_3, schema)
+    end
+
+    test "integer, equal_to: 5" do
+      schema = %{age: [type: :integer, equal_to: 5]}
+
+      data_1 = %{"age" => 5}
+      data_2 = %{"age" => 4}
+
+      assert Goal.validate_params(data_1, schema) == {:ok, %{age: 5}}
+
+      assert {:error, %Ecto.Changeset{errors: [age: _]}} = Goal.validate_params(data_2, schema)
+    end
+
+    test "integer, not_equal_to: 10" do
+      schema = %{age: [type: :integer, not_equal_to: 11]}
+
+      data_1 = %{"age" => 9}
+      data_2 = %{"age" => 11}
+
+      assert Goal.validate_params(data_1, schema) == {:ok, %{age: 9}}
+
+      assert {:error, %Ecto.Changeset{errors: [age: _]}} = Goal.validate_params(data_2, schema)
     end
 
     test "invalid nested map" do
@@ -114,7 +273,7 @@ defmodule GoalTest do
         ]
       }
 
-      assert {:error, %Ecto.Changeset{} = changeset} = Goal.validate(data, schema)
+      assert {:error, %Ecto.Changeset{} = changeset} = Goal.validate_params(data, schema)
 
       assert errors_on(changeset) == %{
                key_1: ["is invalid"],
@@ -128,7 +287,6 @@ defmodule GoalTest do
     end
   end
 
-  @spec errors_on(Ecto.Changeset.t()) :: map
   defp errors_on(changeset) do
     Goal.Changeset.traverse_errors(changeset, fn {message, opts} ->
       Regex.replace(~r"%{(\w+)}", message, fn _map, key ->
