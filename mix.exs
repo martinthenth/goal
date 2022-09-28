f15e10a12a10c6157221432dfca579b2445db848
Martin Nijboer <m.r.nijboer@thables.com>
2022-09-28 20:49:15 +0200

    Bump version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Goal.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @source_url "https://github.com/martinthenth/goal"
   @changelog_url "https://github.com/martinthenth/goal/blob/main/CHANGELOG.md"
 
