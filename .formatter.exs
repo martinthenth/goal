78e1868a12b3b601f7956e52b5c7d98e03b1757b
Nelson Estevão <nelsonmestevao@proton.me>
2024-06-22 12:07:08 +0100

    feat: export formatting rules for macros without parens

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,17 @@
 # Used by "mix format"
+locals_without_parens = [
+  defparams: 1,
+  defparams: 2,
+  optional: 1,
+  optional: 2,
+  optional: 3,
+  required: 1,
+  required: 2,
+  required: 3
+]
+
 [
+  locals_without_parens: locals_without_parens,
+  export: [locals_without_parens: locals_without_parens],
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
 ]
